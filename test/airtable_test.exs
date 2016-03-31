3975d8375441d0e480531f65ce177a88dc45c198
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-31 19:03:08 +0300

    bad, bad, dirty test

---
+++ test/airtable_test.exs
@@ -0,0 +1,10 @@
+defmodule AirtableTest do
+  use PowerAssert
+
+  test "get all rows for table_red records" do
+
+    {:ok, data} = Airtable.fetch_records("table_red")
+    assert (hd data["records"])["fields"] == %{"Name" => "Second element"}
+  end
+
+end
